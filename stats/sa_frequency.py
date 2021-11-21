4046fd2ac0df0015a12aa5db222a28c008f7f79d
Florian Matter <florianmatter@gmail.com>
2021-11-21 09:44:47 +0100

    Sa verb frequency stats

--- stats/sa_frequency.py
+++ stats/sa_frequency.py
@@ -2,13 +2,17 @@ import pandas as pd
 from pyigt.igt import iter_morphemes, IGT, Corpus
 import yaml
 from pycldf.terms import Terms
+import numpy as np
+import scipy
+from scipy.stats import chisquare
 
 
 def check_gloss(obj, gloss):
-    igt = IGT("nil", phrase = obj.split("\t"), gloss=gloss.split("\t"), properties={})
+    igt = IGT("nil", phrase=obj.split("\t"), gloss=gloss.split("\t"), properties={})
     if not igt.is_valid():
         print(igt)
     return igt.glossed_morphemes
+
 
 word_count = 0
 morph_count = 0
@@ -22,11 +26,11 @@ for igt in test:
         for i, (obj, gloss) in enumerate(word):
             morph_count += 1
             if obj in ["at", "os", "e"] and gloss in ["reflex", "reflx_se", "reflx"]:
-                word[i+1] = list(word[i+1])
-                word[i+1][0] = obj + "-" + word[i+1][0]
-                word[i+1][1] = gloss + "-" + word[i+1][1]
+                word[i + 1] = list(word[i + 1])
+                word[i + 1][0] = obj + "-" + word[i + 1][0]
+                word[i + 1][1] = gloss + "-" + word[i + 1][1]
         for obj, gloss in word:
-            key = obj.lower()+":"+gloss.lower()
+            key = obj.lower() + ":" + gloss.lower()
             if key not in found_morphemes:
                 found_morphemes[key] = 1
             else:
@@ -40,20 +44,30 @@ for verb in verbs:
         key = ":".join(form)
         if key in found_morphemes:
             verb_count += found_morphemes[key]
-    counts.append({
-        "count": verb_count,
-        "verb": verb["meaning"],
-        "form": verb["form"],
-    })
-            
+    counts.append(
+        {
+            "Form": verb["form"],
+            "Meaning": verb["meaning"],
+            "Count": verb_count,
+        }
+    )
+
 df = pd.DataFrame.from_dict(counts)
+df["% Sa"] = df["Count"] / sum(df["Count"])
+df["% Words"] = df["Count"] / word_count
+df.sort_values("Count", ascending=False, inplace=True)
+df.to_csv("apalai_sa_verb_stats.csv", index=False)
+print(
+    df.to_string(
+        formatters={
+            "% Sa": "{:,.2%}".format,
+            "% Words": "{:,.2%}".format,
+        }
+    )
+)
 
-df["%\m"] = df["count"] / morph_count
-df["%\w"] = df["count"] / word_count
-df.sort_values("count", ascending=False, inplace=True)
-df.to_csv("sa_verb_stats.csv", index=False)
-print(df.to_string(formatters={
-    "%\w": '{:,.2%}'.format,
-    "%\m": '{:,.2%}'.format,
-}))
-
+avg = sum(df["Count"]) / len(df)
+observed_values = np.array(df["Count"])
+expected_values=np.array([avg] * len(df))
+a, b = chisquare(observed_values, f_exp=expected_values)
+print(a, b)
