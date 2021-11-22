89a5a6ab369add5df873bbe69bcbec65b0060c07
fmatter <florianmatter@gmail.com>
2021-11-22 13:01:20 +0100

    third text, various fixes

--- cldfbench_textos.py
+++ cldfbench_textos.py
@@ -10,12 +10,10 @@ from cldfbench import CLDFSpec, CLDFWriter
 from cldfbench import Dataset as BaseDataset
 import yaml
 
-
-
 class Dataset(BaseDataset):
     dir = pathlib.Path(__file__).parent
     id = "textos_apalai"
-    
+
     def parse_pdf(self):
 
         line_mapping = json.load(open("etc/line_mapping.json"))
@@ -23,8 +21,8 @@ class Dataset(BaseDataset):
         line_splits = json.load(open("etc/line_splits.json"))
         col_mapping = json.load(open("etc/col_mapping.json"))
         unit_check = json.load(open("etc/unit_check.json"))
-        insertions  = json.load(open("etc/insertions.json"))
-        replacements  = json.load(open("etc/replacements.json"))
+        insertions = json.load(open("etc/insertions.json"))
+        replacements = json.load(open("etc/replacements.json"))
         broken_units = json.load(open("etc/broken_units.json"))
 
         segments = pd.read_csv("etc/profile.csv")
@@ -60,20 +58,8 @@ class Dataset(BaseDataset):
                 target_file = f"{text}_{text_page}.pdf"
                 target_path = os.path.join("temp", text_folder, target_file)
                 source_path = f"raw/single_pages/out-{str(total_page).zfill(3)}.pdf"
-                os.popen(f"cp {source_path} {target_path}")
-
-        def print_example(ex):
-            for key in [
-                "ID",
-                "Primary_Text",
-                "Analyzed_Word",
-                "Gloss",
-                "gramm",
-                "Translated_Text",
-            ]:
-                if key in ex:
-                    print(f"{key}:\t{ex[key]}")
-            print("")
+                if not os.path.isfile(target_path):
+                    os.popen(f"cp {source_path} {target_path}")
 
         def identify_units(lines, label, page_break=False):
             units = []
@@ -87,8 +73,74 @@ class Dataset(BaseDataset):
                 if line.strip() == "":
                     continue
                 current_unit.append(line)
+            if len(current_unit) < 2:
+                raise ValueError(f"Empty unit in {label}: {line[0]}")
             units.append(current_unit)
             return units
+
+        def pad_ex(obj, gloss):
+            out_obj = []
+            out_gloss = []
+            for o, g in zip(obj.split(" "), gloss.split(" ")):
+                diff = len(o) - len(g)
+                if diff < 0:
+                    o += " " * -diff
+                else:
+                    g += " " * diff
+                out_obj.append(o)
+                out_gloss.append(g)
+            return "  ".join(out_obj).strip(" "), "  ".join(out_gloss).strip(" ")
+
+        def format_example(unit):
+            obj, gloss = pad_ex(unit["Analyzed_Word"], unit["Gloss"])
+            return f"""{unit["ID"]}\n{unit["Primary_Text"]}\n{obj}\n{gloss}\n{unit["Translated_Text"]}"""
+
+        def export_page_examples(page_id, units, page_file):
+            out = [format_example(unit) for unit in units]
+            f = open(page_file, "w")
+            f.write("\n\n".join(out))
+            f.close()
+            return None
+        
+        def check_igt(unit):
+            igt = IGT(
+                id=unit["ID"],
+                gloss=unit["Gloss"].split(" "),
+                phrase=unit["Analyzed_Word"].split(" "),
+                language="apa",
+            )
+            if not igt.is_valid():
+                print(f"""WORD MISMATCH""")
+                exit()
+            for i, (w, m) in enumerate(zip(igt.phrase_segmented, igt.gloss_segmented), start=1):
+                if len(w) != len(m):
+                    print(f"""MORPHEME MISMATCH""")
+                    print("-".join(w))
+                    print("-".join(m))
+                    print("---")
+                    exit()
+                        
+        def print_example(ex):
+            for key in [
+                "ID",
+                "Primary_Text"
+            ]:
+                if key in ex:
+                    print(f"{key}:\t{ex[key]}")
+            remaining_keys = [
+                "gramm",
+                "Translated_Text",
+            ]
+            if "Analyzed_Word" in ex and "Gloss" in ex:
+                obj, gloss = pad_ex(ex["Analyzed_Word"], ex["Gloss"])
+                print(obj, gloss, sep="\n")
+            else:
+                remaining_keys.append("Analyzed_Word")
+                remaining_keys.append("Gloss")
+            for key in remaining_keys:
+                if key in ex:
+                    print(f"{key}:\t{ex[key]}")
+            print("")
 
         def print_partial_analysis(unit_raw, unit, keys):
             for i, l in enumerate(unit_raw):
@@ -97,7 +149,7 @@ class Dataset(BaseDataset):
                     print("")
             print_example(unit)
 
-        def parse_unit(unit_raw, label, text):
+        def parse_unit(unit_raw, label, text, page):
             # print("\n".join(unit_raw))
             if label not in unit_raw[0]:
                 return None
@@ -109,7 +161,7 @@ class Dataset(BaseDataset):
             keys = ["Primary_Text", "Analyzed_Word", "Gloss", "gramm"]
             specific_keys = {"Translated_Text": -1}
             if id not in unit_check or unit_check[id]["initial"] != 1:
-                print("Input:")
+                print(f"Input ({page}):")
                 print_partial_analysis(unit_raw, unit, keys)
 
             if unit["ID"] in line_mapping:
@@ -143,10 +195,10 @@ class Dataset(BaseDataset):
                                 unit[key] = unit_raw.pop(0)
                             else:
                                 unit[key] += " " + unit_raw.pop(0)
-                else:
-                    print(f"Length mismatch in {id} ({len(unit_raw)}):")
-                    print_partial_analysis(unit_raw, unit, keys)
-                    # raise ValueError("Invalid number of lines in unit")
+                # else:
+                # print(f"Length mismatch in {id} ({len(unit_raw)}):")
+                # print_partial_analysis(unit_raw, unit, keys)
+                # raise ValueError("Invalid number of lines in unit")
             for key in unit.keys():
                 if key == "Gloss":
                     unit[key] = (
@@ -158,17 +210,48 @@ class Dataset(BaseDataset):
                     )
                     for orig, repl in gloss_corr.items():
                         unit[key] = unit[key].replace(orig, repl)
-                unit[key] = unit[key].replace("  ", " ").replace("AAA", "***").strip()
+                unit[key] = (
+                    unit[key]
+                    .replace("  ", " ")
+                    .replace("aaa", "***")
+                    .replace("AAA", "***")
+                    .strip()
+                )
                 if key in ["Gloss", "Analyzed_Word"]:
                     unit[key] = unit[key].replace("- ", "-").replace(" -", "-")
                 if key in ["Analyzed_Word", "Primary_Text"]:
                     unit[key] = unit[key].translate(
                         {ord(x): y for x, y in obj_corr.items()}
                     )
+            
+            if id in insertions:
+                for key, inserts in insertions[id].items():
+                    key = col_mapping[key]
+                    for position, add_text in inserts.items():
+                        pos = int(position)
+                        # print(f"""Inserting {add_text} in {key} field at position {pos} in unit {parsed_unit["ID"]}""")
+                        unit[key] = (
+                            unit[key][:pos]
+                            + add_text
+                            + unit[key][pos:]
+                        )
+            if id in replacements:
+                for key, repl in replacements[id].items():
+                    for a, b in repl.items():
+                        # print(f"Replacing {a} with {b} in {key} in {unit_id}")
+                        unit[col_mapping[key]] = unit[
+                            col_mapping[key]
+                        ].replace(a, b)
+                        
             if id not in unit_check:
                 print_example(unit)
-                val = input("Is parsing good? [y]es [N]o\n")
-                if val != "y":
+                check_igt(unit)
+                val = input("Is parsing good? [y]es [N]o [x] save and exit\n")
+                if val == "x":
+                    with open("etc/unit_check.json", "w") as outfile:
+                        json.dump(unit_check, outfile, indent=2)
+                    exit
+                elif val != "y":
                     return None
                 else:
                     if id not in unit_check:
@@ -184,9 +267,10 @@ class Dataset(BaseDataset):
             string = string.translate({ord(x): "" for x in delim})
             return tokenizer(string).replace(" ", "").replace("#", " ")
 
-        parsed = []
+        parsed = {}
 
         for text, metadata in texts.items():
+            parsed[text] = {}
             if metadata["Label"]:
                 text_label = metadata["Label"]
             else:
@@ -200,19 +284,16 @@ class Dataset(BaseDataset):
                 page_map[text_page + 1] = total_page
             if not os.path.isdir(text_folder):
                 get_pages(text, text_folder, page_map)
-            if text not in ["po2", "ner1"]:
-                continue
             page_break = False
             for text_page, total_page in page_map.items():
                 page_id = f"{text}_{text_page}"
+                parsed[text][page_id] = []
                 print(page_id)
                 page_pdf = os.path.join("temp", text_folder, f"{page_id}.pdf")
                 raw_text = parser.from_file(page_pdf)
                 text_file = os.path.join("temp", text_folder, f"{page_id}.txt")
+                parsed_text_file = os.path.join("temp", text_folder, f"{page_id}_p.txt")
                 lines = raw_text["content"].split("\n")
-                if page_id in line_moves:
-                    for move in line_moves[page_id]:
-                        lines.insert(move[1] - 1, lines.pop(move[0]))
                 if page_id in line_splits:
                     for lineno, indices in line_splits[page_id].items():
                         indices = [0] + indices
@@ -223,6 +304,13 @@ class Dataset(BaseDataset):
                         del lines[int(lineno)]
                         for new_line in reversed(split_line):
                             lines.insert(int(lineno), new_line)
+                if page_id in replacements:
+                    for line, repls in replacements[page_id].items():
+                        for orig, new in repls.items():
+                            lines[int(line)] = lines[int(line)].replace(orig, new)
+                if page_id in line_moves:
+                    for move in line_moves[page_id]:
+                        lines.insert(move[1], lines.pop(move[0]))
                 f = open(text_file, "w")
                 for i, line in enumerate(lines):
                     f.write(f"{i} {line}\n")
@@ -236,26 +324,13 @@ class Dataset(BaseDataset):
                         if page_break:
                             unit = partial_unit_1 + unit
                             page_break = False
-                        parsed_unit = parse_unit(unit, text_label, text)
-                        # print_example(parsed_unit)
+                        parsed_unit = parse_unit(unit, text_label, text, page_id)
                         if not parsed_unit:
                             continue
                         if page_break:
                             print(parsed_unit, partial_unit_1)
-                        parsed_unit["page"] = total_page
+                        parsed_unit["page"] = text_page
                         unit_id = parsed_unit["ID"]
-                        if parsed_unit["ID"] in insertions:
-                            for key, inserts in insertions[parsed_unit["ID"]].items():
-                                key = col_mapping[key]
-                                for position, add_text in inserts.items():
-                                    pos = int(position)
-                                    # print(f"""Inserting {add_text} in {key} field at position {pos} in unit {parsed_unit["ID"]}""")
-                                    parsed_unit[key] = parsed_unit[key][:pos] + add_text + parsed_unit[key][pos:]
-                        if unit_id in replacements:
-                            for key, repl in replacements[unit_id].items():
-                                for a, b in repl.items():
-                                    # print(f"Replacing {a} with {b} in {key} in {unit_id}")
-                                    parsed_unit[col_mapping[key]] = parsed_unit[col_mapping[key]].replace(a, b)
                         if "Primary_Text" in parsed_unit:
                             parsed_unit["pnm"] = ipaify(parsed_unit["Primary_Text"])
                             if "Analyzed_Word" in parsed_unit:
@@ -263,7 +338,9 @@ class Dataset(BaseDataset):
                                     parsed_unit["Analyzed_Word"], obj=True
                                 )
                                 text_length += 1
-                                parsed.append(parsed_unit)
+                                parsed[text][page_id].append(parsed_unit)
+                export_page_examples(page_id, parsed[text][page_id], parsed_text_file)
+
             texts[text]["length"] = text_length
 
         def add_sample(cand, indices, lim):
@@ -277,23 +354,30 @@ class Dataset(BaseDataset):
             mod = 0
             while len(indices) < 7:
                 mods = [
-                    lambda x: lim-x,
-                    lambda x: (lim-x)//2,
-                    lambda x: (lim-x)//2+5,
-                    lambda x: (lim-x)+10,
-                    lambda x: (lim-x)//3,
-                    lambda x: (lim-x)//3+2,
+                    lambda x: lim - x,
+                    lambda x: (lim - x) // 2,
+                    lambda x: (lim - x) // 2 + 5,
+                    lambda x: (lim - x) + 10,
+                    lambda x: (lim - x) // 3,
+                    lambda x: (lim - x) // 3 + 2,
                 ]
                 for c in text:
                     indices = add_sample(mods[mod](ord(c)), indices, lim)
-                if mod >= len(mods)-1:
+                if mod >= len(mods) - 1:
                     mod = 0
                 else:
                     mod += 1
             return indices
-            
-        df = pd.DataFrame.from_dict(parsed)
+
+        all_parsed = []
+        for pages in parsed.values():
+            for units in pages.values():
+                for unit in units:
+                    all_parsed.append(unit)
+
+        df = pd.DataFrame.from_dict(all_parsed)
         df["Language_ID"] = "apa"
+        df["Source"] = "koehns1994textos"
         df["Analyzed_Word"] = df["Analyzed_Word"].apply(
             lambda x: "\t".join(x.split(" "))
         )
@@ -311,22 +395,21 @@ class Dataset(BaseDataset):
         )
         with open("etc/unit_check.json", "w") as outfile:
             json.dump(unit_check, outfile, indent=2)
-            
+
     def cldf_specs(self):  # A dataset must declare all CLDF sets it creates.
         return {
-            'full': CLDFSpec(
+            "full": CLDFSpec(
                 dir=self.cldf_dir,
-                module='Generic',
-                data_fnames={'ExampleTable': 'examples_full.csv'},
+                module="Generic",
                 writer_cls=CLDFWriter,
-                metadata_fname="metadata_full.json"
+                metadata_fname="metadata_full.json",
             ),
-            'sampled': CLDFSpec(
+            "sampled": CLDFSpec(
                 dir=self.cldf_dir,
-                module='Generic',
-                data_fnames={'ExampleTable': 'examples.csv'},
+                module="Generic",
+                data_fnames={"ExampleTable": "examples.csv"},
                 writer_cls=CLDFWriter,
-                metadata_fname="metadata.json"
+                metadata_fname="metadata.json",
             ),
         }
 
@@ -339,13 +422,15 @@ class Dataset(BaseDataset):
         >>> self.raw_dir.download(url, fname)
         """
         # pass
-    
+
     def cmd_makecldf(self, args):
+        sources = self.etc_dir.read_bib("apalai.bib")
         with self.cldf_writer(args, cldf_spec="sampled") as writer:
             writer.cldf.add_component("LanguageTable")
 
             writer.cldf.remove_columns("ExampleTable", "Gloss", "Analyzed_Word")
-            writer.cldf.add_columns("ExampleTable",
+            writer.cldf.add_columns(
+                "ExampleTable",
                 "Text_ID",
                 "Example_ID",
                 "Comments",
@@ -355,7 +440,16 @@ class Dataset(BaseDataset):
                 "pnm_parsed",
                 {"name": "Sentence_Number", "datatype": "integer"},
                 {"name": "Phrase_Number", "datatype": "integer"},
-                 {
+            {
+                "name": "Source",
+                "required": False,
+                "propertyUrl": "http://cldf.clld.org/v1.0/terms.rdf#source",
+                "datatype": {
+                    "base": "string"
+                },
+                "separator": ";"
+            },
+                {
                     "dc:description": "The sequence of words of the primary text to be aligned with glosses",
                     "dc:extent": "multivalued",
                     "datatype": "string",
@@ -372,10 +466,12 @@ class Dataset(BaseDataset):
                     "required": False,
                     "separator": "\t",
                     "name": "Gloss",
-                })
-            writer.cldf.add_table("texts.csv", "ID", "Title")
+                },
+            )
+            writer.cldf.add_table("texts.csv", "ID", "Title", **{"primaryKey": "ID"})
             writer.cldf.add_foreign_key("ExampleTable", "Text_ID", "texts.csv", "ID")
-            
+            writer.cldf.add_sources(*sources)
+
             writer.objects["LanguageTable"].append(
                 {"ID": "apa", "Name": "Apalaí", "Glottocode": "apal1257"}
             )
@@ -384,15 +480,16 @@ class Dataset(BaseDataset):
                 writer.objects["texts.csv"].append(
                     {"ID": row["ID"], "Title": row["Title"]}
                 )
+                
             LanguageTable = writer.cldf["LanguageTable"]
             ExampleTable = writer.cldf["ExampleTable"]
             TextTable = writer.cldf["texts.csv"]
-            
+
         with self.cldf_writer(args, cldf_spec="full", clean=False) as writer:
-            writer.cldf.remove_table("ExampleTable")
             ExampleTable.url = "examples_full.csv"
             writer.cldf.add_component(ExampleTable)
             writer.cldf.add_component(TextTable)
             writer.cldf.add_component(LanguageTable)
-
+            writer.cldf.add_sources(*sources)
+        
         self.parse_pdf()
